ce944f971c74158577cb2a0192865bbaa926df9c
Amber Lee <lee.amber.alex@gmail.com>
2020-11-16 17:18:24 -0800

    reorganized directories, working notes html, added note id and delete functions.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,36 @@
 const savedNotes = require("../db/db.json");
 const path = require("path");
-
-
+const fs = require("fs");
+const nanoID = require("nanoid").nanoid;
+console.log(savedNotes);
 module.exports = function (app) {
-
   app.get("/api/notes", function (req, res) {
     return res.json(savedNotes);
   });
-};
 
-// app.post("/api/notes", function(req, res) {
-// // convert db.json into an array, then push new not into the array
-// // 
-    
-//   });
+  app.post("/api/notes", function (req, res) {
+    const tempDB = savedNotes;
+    // convert db.json into an array, then push new not into the array
+    const newNote = {
+      id: nanoID(),
+      ...req.body,
+    };
+    tempDB.push(newNote);
+    fs.writeFile("app/db/db.json", JSON.stringify(tempDB), function () {
+      res.json(tempDB);
+    });
+  });
+  app.delete("/api/notes/:id", function (req, res) {
+    const tempDB = savedNotes;
+
+    const deleteIndex = tempDB.findIndex(
+      (element) => element.id === req.params.id
+    );
+
+    tempDB.splice(deleteIndex, 1);
+
+    fs.writeFile("app/db/db.json", JSON.stringify(tempDB), function () {
+      res.json(tempDB);
+    });
+  });
+};
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require("fs");
 const savedNotes = require("./app/db/db.json");
 const port = process.env.Port || 8080;
 
 
-app.use(express.static("public"));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("app/public"));
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
